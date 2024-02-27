652f3293e64040bf2cbabd3e583ceab189cc42ed
Vanessa Rice <vanessa.rice@keyin.com>
2024-02-27 12:16:33 -0330

    updated server.js

--- FSJS/server.js
+++ FSJS/server.js
@@ -1,67 +1,57 @@
 const express = require("express");
-const fs = require("fs");
-const { parse } = require("querystring");
+const bodyParser = require("body-parser");
 const { newToken, tokenCount } = require("./userToken");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the 'views' directory
 app.use(express.static('views'));
 
-// Handle POST requests to '/new'
+// Route handler for GET requests to "/new"
+app.get('/new', (req, res) => {
+  res.sendFile(__dirname + '/views/newusertoken.html');
+});
+
+// Route handler for POST requests to "/new"
 app.post('/new', (req, res) => {
-  collectRequestData(req, (result) => {
-    newToken(result.username, (error, theToken) => {
-      if (error) {
-        console.error("Error generating token:", error);
-        res.statusCode = 500;
-        res.end("Error generating token");
-      } else {
-        res.end(`
-          <!doctype html>
-          <html>
-          <body>
-              ${result.username} token is ${theToken} <br />
-              <a href="http://localhost:3000">[home]</a>
-          </body>
-          </html>
-        `);
-      }
-    });
+  const username = req.body.username; // Assuming a form field named 'username'
+  newToken(username, (error, theToken) => {
+    if (error) {
+      console.error("Error generating token:", error);
+      res.status(500).send("Error generating token");
+    } else {
+      res.send(`
+        <!doctype html>
+        <html>
+        <body>
+            ${username} token is ${theToken} <br />
+            <a href="/">[home]</a>
+        </body>
+        </html>
+      `);
+    }
   });
 });
 
-// Handle GET requests to '/count'
+// Route handler for GET requests to "/count"
 app.get('/count', async (req, res) => {
-  var theCount = await tokenCount();
-  res.end(`
+  const theCount = await tokenCount();
+  res.send(`
     <!doctype html>
     <html>
     <body>
         Token count is ${theCount} <br />
-        <a href="http://localhost:3000">[home]</a>
+        <a href="/">[home]</a>
     </body>
     </html>
   `);
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-function collectRequestData(request, callback) {
-  const FORM_URLENCODED = "application/x-www-form-urlencoded";
-  if (request.headers["content-type"] === FORM_URLENCODED) {
-    let body = "";
-    request.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    request.on("end", () => {
-      callback(parse(body));
-    });
-  } else {
-    callback(null);
-  }
-}
